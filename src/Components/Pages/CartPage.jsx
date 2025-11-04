7918276bae13d99161d53e3daabc4ee03a992ec1
Amina SALMI <asalm@lmdsio.com>
2025-11-04 10:50:26 +0100

    V3

--- src/Components/Pages/CartPage.jsx
+++ src/Components/Pages/CartPage.jsx
@@ -1,7 +1,8 @@
 import Cart from '../../Components/Cart';
 import { Link } from 'react-router-dom';
+import '../../styles/CartPage.css';
 
-function CartPage({ cart, removeFromCart, clearCart }){
+function CartPage({ cart, removeFromCart, clearCart }) {
     return (
         <div>
             <Cart
@@ -11,10 +12,12 @@ function CartPage({ cart, removeFromCart, clearCart }){
             />
 
             {cart.length > 0 && (
-                <div>
+                <div className='cart'>
+                <div className="checkout-button-container">
                     <Link to="/checkout">
                         <button>Passer la commande</button>
                     </Link>
+                </div>
                 </div>
             )
             }
